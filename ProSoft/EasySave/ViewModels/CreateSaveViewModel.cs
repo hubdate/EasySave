8ad7b29993fb6d0a9453cf9bc8ee2c257cb140f7
Yan ADAM <yan.adam@viacesi.fr>
2023-12-18 10:37:33 +0100

    Ajouts fonctionnalitées page de création de sauvegarde

--- ProSoft/EasySave/ViewModels/CreateSaveViewModel.cs
+++ ProSoft/EasySave/ViewModels/CreateSaveViewModel.cs
@@ -1,10 +1,15 @@
 using System.Reactive;
 using ReactiveUI;
+using Avalonia.Controls;
+using Avalonia;
+using System;
 
 namespace EasySave.ViewModels;
 
 public class CreateSaveViewModel : ViewModelBase
 {
+    private readonly IDialogService _dialogService;
+    private readonly Window _mainWindow;
     private bool _isTextReadOnly = true;
 
     public bool IsTextReadOnly
@@ -19,13 +24,67 @@ public class CreateSaveViewModel : ViewModelBase
 
     public ReactiveCommand<Unit, Unit> ToggleReadOnlyCommand { get;}
 
-    public CreateSaveViewModel()
+    public CreateSaveViewModel(IDialogService dialogService, Window mainWindow)
     {
         ToggleReadOnlyCommand = ReactiveCommand.Create(ToggleReadOnly);
+        _dialogService = dialogService;
+        _mainWindow = mainWindow;
     }
 
     private void ToggleReadOnly()
     {
         IsTextReadOnly = !IsTextReadOnly;
     }
-}  
+
+    public async void OpenFileExplorer()
+    {
+        var result = await _dialogService.ShowOpenFileDialogAsync();
+
+        if (result != null)
+        {
+            // Do something with the selected files
+        }
+    }
+
+    public async void OpenFolderExplorer()
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        };
+        var result = await dialog.ShowAsync(_mainWindow);
+
+        if (result != null)
+        {
+            // Do something with the selected folder
+        }
+    }
+
+    public async void OpenOSFolderExplorer()
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Directory = Environment.GetFolderPath(Environment.SpecialFolder.System)
+        };
+        var result = await dialog.ShowAsync(_mainWindow);
+
+        if (result != null)
+        {
+            // Do something with the selected folder
+        }
+    }
+
+    public async void OpenAppDataFolderExplorer()
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Directory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        };
+        var result = await dialog.ShowAsync(_mainWindow);
+
+        if (result != null)
+        {
+            // Do something with the selected folder
+        }
+    }
+}
